c4a6fb456a8fb500a2abebf86c80ce7e30101c55
zhoud9012 <zhoud9012@gmail.com>
2018-04-14 19:54:35 +0800

    设置控制器返回格式为json

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace app\controllers;
+use yii\web\Response;
 
 class BookController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\Book';
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+        $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
+        return $behaviors;
+    }
 }
