b5945b6ca4024a231187b5b506ecbe1e34242941
zhoud01@mingyuanyun.com <zhoud01@mingyuanyun.com>
2018-04-16 17:06:29 +0800

    增加CandidatesInfoController config 配置

--- app/config/main.php
+++ app/config/main.php
@@ -66,6 +66,14 @@ return [
                         'POST search' => 'search' //http 动词 参数    动作名
                     ],
                 ],
+                ['class'=>'yii\rest\UrlRule',
+                    'controller'=>'candidates-info',
+                    'except'=>['delete','create','update','view'],//禁用的http动词
+                    'pluralize'=>false,
+                    'extraPatterns'=>[// 为方法配置restful 请求
+                        'POST search' => 'search' //http 动词 参数    动作名
+                    ],
+                ],
             ],
         ],
 
