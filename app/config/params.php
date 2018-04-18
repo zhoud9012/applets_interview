ac8f9abfe9013d65662ea60d82f44a6365ce3d7b
zhoud9012 <zhoud9012@gmail.com>
2018-04-18 09:54:25 +0800

    1.增加自己的配置

--- app/config/params.php
+++ app/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     //'adminEmail' => 'admin@example.com',
-    "oss" => [
+    /*"oss" => [
         'ossServer' => 'http://oss-cn-shenzhen.aliyuncs.com', //服务器外网地址，深圳为 http://oss-cn-shenzhen.aliyuncs.com
         'ossServerInternal' => 'http://oss-cn-shenzhen-internal.aliyuncs.com', //服务器内网地址，深圳为 http://oss-cn-shenzhen-internal.aliyuncs.com
         "AccessKeyId" => '61vs8qNHuFvbNF5Y',
@@ -11,5 +11,17 @@ return [
     "uploader" => [
         "type" => "oss", //oss
         "rootPath" => "mybackend"//weiloushu
+    ],*/
+
+   "oss" => [
+        'ossServer' => 'http://oss-cn-shenzhen.aliyuncs.com', //服务器外网地址，深圳为 http://oss-cn-shenzhen.aliyuncs.com
+        'ossServerInternal' => 'http://oss-cn-shenzhen-internal.aliyuncs.com', //服务器内网地址，深圳为 http://oss-cn-shenzhen-internal.aliyuncs.com
+        "AccessKeyId" => 'LTAIz5Zz0fftzT0B',
+        "AccessKeySecret" => 'zgP07kjQrL3mCJG2YEJO3PLA0tSt9L',
+        'Bucket' => 'testzd',
+    ],
+    "uploader" => [
+        "type" => "oss", //oss
+        "rootPath" => "testTemp"//weiloushu
     ],
 ];
