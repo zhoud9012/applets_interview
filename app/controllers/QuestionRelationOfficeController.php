7080cb79e121e57101b6b1c23bac4fe4498d38aa
zhoud9012 <zhoud9012@gmail.com>
2018-04-21 20:47:44 +0800

    1.新增问题控制器 2.新增问题关系控制器

---
+++ app/controllers/QuestionRelationOfficeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class QuestionRelationOfficeController extends \BaseController
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
